69a858627da474708ac3563f8ecd5a87288971dc
cloudPC <iotacecil@gmail.com>
2019-01-15 20:06:06 +0800

    eject test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {createStore} from 'redux'
+import { Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css'
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+            <Button type = "primary" onClick={()=>this.addSolder()}>bbb</Button>
             <p>editedit{boss}</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
